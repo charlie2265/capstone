cd2741d77d6145f21cbee05923b033380c73d4c1
Charlie <charlie2265@gmail.com>
2019-02-28 12:18:36 -0800

    capstone 2/28

--- employee/urls.py
+++ employee/urls.py
@@ -7,4 +7,6 @@ app_name = 'employee'
 urlpatterns = [
     path('staff', views.staff_panel, name='staff_panel'),
     path('employee', views.employee_panel, name='employee_panel'),
+    path('add_employee', views.add_employee, name='add_employee'),
+    path('remove_employee',views.remove_employee, name='remove_employee')
 ]
--- employee/views.py
+++ employee/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import logout
 
 # Create your views here.
 def employee_panel(request):
     return render(request, 'employee/employee_panel.html')
 
 def staff_panel(request):
+    users = User.objects.all()
     # link to go to a submit user view 
-    # show existing users 
     # link to remove users
-    # logout
-    return render(request, 'employee/staff_panel.html')    
+    return render(request, 'employee/staff_panel.html', {'users': users})    
+
     
 def add_employee(request):
     
-    pass
-
+    return render(request, 'add_remove/add_employee.html') 
+    
 def remove_employee(request):
-    pass
+    
+    return render(request, 'add_remove/remove_employee.html')
